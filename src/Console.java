83311bc76891936fe88f1a6843c37dd7f68c7543
AntonMestre <anmaystre@gmail.com>
2022-01-20 09:00:28 +0100

    changement

--- src/Console.java
+++ src/Console.java
@@ -13,8 +13,48 @@ public class Console {
 		System.out.println("Quel sera le nom de votre Hotel ?");
 	}
 	
+	public static void afficherChoixCreationCheminAcces() {
+		System.out.println("Quel sera le chemin d'accès du modele ?");
+	}
+	
 	public static void afficherChoixCheminAcces() {
 		System.out.println("Quel sera le chemin d'accès du modele ?");
+	}
+	
+	public static void afficherChoixPrincicpaux() {
+		System.out.println("Bienvenue dans l'hotel : " + hotel.getNom());
+		
+		System.out.println("Que voulez vous faire ?\n");
+		
+		System.out.println("---------[ RESERVATION ]----------");
+		System.out.println("11 - Créer une RESERVATION :");
+		System.out.println("12 - Modifier une RESERVATION :");
+		System.out.println("13 - Supprimer une RESERVATION :");
+		System.out.println("14 - Afficher une RESERVATION :");
+		System.out.println("15 - Afficher toutes les RESERVATION :");
+		
+		System.out.println("---------[ CHAMBRE ]----------");
+		System.out.println("21 - Créer une CHAMBRE :");
+		System.out.println("22 - Modifier une CHAMBRE :");
+		System.out.println("23 - Supprimer une CHAMBRE :");
+		System.out.println("24 - Afficher une CHAMBRE :");
+		System.out.println("25 - Afficher toutes les CHAMBRE :");
+
+		System.out.println("---------[ PERSONNEL ]----------");
+		System.out.println("31 - Créer un PERSONNEL :");
+		System.out.println("32 - Modifier un PERSONNEL :");
+		System.out.println("33 - Supprimer un PERSONNEL :");
+		System.out.println("34 - Afficher un PERSONNEL :");
+		System.out.println("35 - Afficher tout le PERSONNEL :");
+		
+		System.out.println("---------[ CLIENT ]----------");
+		System.out.println("41 - Créer un CLIENT :");
+		System.out.println("42 - Modifier un CLIENT :");
+		System.out.println("43 - Supprimer un CLIENT :");
+		System.out.println("44 - Afficher un CLIENT :");
+		System.out.println("45 - Afficher tous les CLIENT :");
+		
+		System.out.println("\n5 - Quitter et enregistrer");
 	}
 	
 	public static String recupererUneEntree() {
